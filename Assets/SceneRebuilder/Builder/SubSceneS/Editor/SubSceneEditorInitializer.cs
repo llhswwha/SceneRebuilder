c5058985cfa07143696dc4092148ea6537f19632
caiwanwei <llhswwha@gmail.com>
2021-06-22 09:19:37 +0800

    添加文件夹ModelInfos

--- Assets/SceneRebuilder/Builder/SubSceneS/Editor/SubSceneEditorInitializer.cs
+++ Assets/SceneRebuilder/Builder/SubSceneS/Editor/SubSceneEditorInitializer.cs
@@ -21,6 +21,15 @@ public class SubSceneEditorInitializer
         return r;
     }
 
+    public static void CreateLabel(string text,Rect selectionRect,float width,Color color1,Color color2)
+    {
+        Rect r = CreateRect(selectionRect, width);
+        var style = new GUIStyle();
+        style.normal.textColor = color1;
+        style.hover.textColor = color2;
+        GUI.Label(r, text, style);
+    }
+
     private static void hierarchyOnGUI(int instancedId,Rect selectionRect)
     {
         //EditorUtility.Hi
@@ -43,26 +52,45 @@ public class SubSceneEditorInitializer
                 {
                     style.normal.textColor = Color.yellow;
                 }
-                
                 style.hover.textColor = Color.green;
+
                 //GUI.Label(r, $"[Scene][{isLoaded}]", style);
                 GUI.Label(r, $"[Scene]", style);
             }
             else if (go.GetComponent<ModelAreaTree>() != null)
             {
-                Rect r = CreateRect(selectionRect, 55);
-                var style = new GUIStyle();
-                style.normal.textColor = Color.green;
-                style.hover.textColor = Color.blue;
-                GUI.Label(r, "[ModelTree]", style);
+                CreateLabel("[ModelTree]", selectionRect, 65, Color.green, Color.blue);
             }
             else if (go.GetComponent<AreaTreeNode>() != null)
             {
-                Rect r = CreateRect(selectionRect, 55);
-                var style = new GUIStyle();
-                style.normal.textColor = new Color(0, 0.5f, 1);
-                style.hover.textColor = Color.red;
-                GUI.Label(r, "[TreeNode]", style);
+                CreateLabel("[TreeNode]", selectionRect, 65, new Color(0, 0.5f, 1), Color.red);
+            }
+
+            else if (go.GetComponent<BuildingModelInfo>() != null)
+            {
+                if (go.GetComponent<BuildingController>() != null)
+                {
+                    CreateLabel("[Building][Model]", selectionRect, 90, new Color(1, 0.5f, 0.5f), Color.red);
+                }
+                else
+                {
+                    CreateLabel("[Model]", selectionRect, 90, new Color(1, 0.5f, 1), Color.red);
+                }
+            }
+            else if (go.GetComponent<BuildingController>() != null)
+            {
+                CreateLabel("[Building]", selectionRect, 90, new Color(0.4f, 0.5f, 1), Color.red);
+            }
+            else if (go.GetComponent<FloorController>() != null)
+            {
+                CreateLabel("[Floor]", selectionRect, 65, new Color(0.6f, 0.5f, 0.8f), Color.red);
+            }
+            else
+            {
+                if(go.name=="In" || go.name == "Out1" || go.name == "Out0")
+                {
+                    CreateLabel("[Part]", selectionRect, 65, Color.cyan, Color.red);
+                }
             }
         }
     }
