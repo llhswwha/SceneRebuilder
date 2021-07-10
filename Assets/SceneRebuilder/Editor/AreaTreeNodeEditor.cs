4c72684bf965f46b8edb504fce880fc8600c0b59
caiwanwei <llhswwha@gmail.com>
2021-07-10 08:34:28 +0800

    Editor

---
+++ Assets/SceneRebuilder/Editor/AreaTreeNodeEditor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AreaTreeNode))]
+public class AreaTreeNodeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
